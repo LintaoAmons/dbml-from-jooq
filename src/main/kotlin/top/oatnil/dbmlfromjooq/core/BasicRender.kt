ebe2092c6c7f583a23db31a82039369a4f0eb495
Lintao <Lintao.Amons@outlook.com>
2022-08-06 22:01:42 +0800

    fix render

--- src/main/kotlin/top/oatnil/dbmlfromjooq/core/BasicRender.kt
+++ src/main/kotlin/top/oatnil/dbmlfromjooq/core/BasicRender.kt
@@ -3,15 +3,15 @@ package top.oatnil.dbmlfromjooq.core
 
 class BasicRender : Render {
     override fun render(dbml: DBML): String {
-        return "\nTABLE ${dbml.tableName} {\n ${dbml.columns?.render()} }"
+        return "TABLE ${dbml.tableName} {\n ${dbml.columns?.render()} \n}\n"
     }
 
     private fun List<Column>.render(): String {
-        return this.map { it.renderColumn() }.joinToString("\n")
+        return this.joinToString("\n") { it.renderColumn() }
     }
 
     private fun Column.renderColumn(): String {
-        return "$name $dataValue [note: \"sample data: $dataValue\"]"
+        return "$name $dataType [note: \"sample data: $dataValue\"]"
     }
 
 }
